27de518e2d9865a5ae1a7fbd16985c0c4b648c02
Dupeljan Linux <dupeljan@gmail.com>
2019-09-08 11:28:45 +0300

    done first hometask

---
+++ geometry.pl
@@ -0,0 +1,3 @@
+vertical(segment(point(X,_),point(X,_))).
+horizontal(segment(point(_,Y),point(_,Y))).
+segment(point(2,3),point(1,1)).
--- relatives.pl
+++ relatives.pl
@@ -8,28 +8,56 @@ parent(john,jane).
 parent(sue,bob).
 parent(sue,mark).
 parent(john,jim).
+parent(nick,liza).
+parent(nick,masha).
+
+parent(petya,vova).
+parent(petya,pasha).
+parent(vova,dima).
+parent(vova,ruslan).
+
 man(tom).
 man(mike).
 man(john).
 man(nick).
 man(jim).
+man(petya).
+man(vova).
+man(pasha).
+man(dima).
+man(ruslan).
+
 woman(mary).
 woman(ann).
 woman(sue).
 woman(jane).
+woman(liza).
+woman(masha).
+
+maried(petya,jane).
+maried(dima,sue).
+maried(ruslan,masha).
+maried(pasha,liza).
+
 grandchild(X,Y):-parent(Z,X),parent(Y,Z).
 haschild(X):-parent(X,_).
 sister(X,Y):-woman(X),parent(Z,X),parent(Z,Y),dif(X,Y).
 %ancestor(X,Y):-parent(X,Y).
 %ancestor(X,Y):-parent(Z,Y),ancestor(X,Z).
+ancestor(X,X).
 ancestor(X,Y):-parent(X,Y).
 ancestor(X,Y):-parent(X,Z),ancestor(Z,Y).
 
 patriach(X):-parent(X,_),not(parent(_,X)).
-cousin(X,Y):-parent(Z,X),parent(W,Y),dif(Z,W),parent(O,Z),parent(O,W),dif(X,Y).
-% Родственники
-relative(X,Y):-parent(Px,X),parent(Py,Y),not(dif(Px,Py)).
+cousin(X,Y):-parent(Z,X),parent(W,Y),dif(Z,W),parent(O,Z),parent(O,W),dif(X,Y). %Двоюродные братья или сестры
+% Родственники.
+%relative(X,Y):-parent(Y,X) .
+%relative(X,Y):-parent(X,Y).
+%relative(X,Y):-parent(X,Z),relative(Z,Y),dif(X,Y).
+%relative(X,Y):-parent(Y,Z),relative(Z,X),dif(X,Y).
+relative(X,Y):-ancestor(Z,X),ancestor(Z,Y).
 % женаты на сестрах
+maried_on_sisters(X,Y):-maried(X,Xwife),maried(Y,Ywife),woman(Xwife),sister(Xwife,Ywife).
 
 
 
